b414c83668b368f30c3e9eaea6964846e6e950cc
Richard Alam <ritzalam@gmail.com>
2017-08-20 07:23:04 -0700

    first commit

---
+++ parsnip/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import TasksPage from './components/TasksPage';
+import { createTask, editTask } from './actions';
+
+class App extends Component {
+
+  render() {
+    console.log('props from App: ', this.props)
+    return (
+      <div className="main-content">
+        <TasksPage 
+          tasks={this.props.tasks} 
+          onCreateTask={this.onCreateTask}
+        />
+      </div>
+    );
+  }
+
+  onCreateTask = ({ title, description }) => {
+    this.props.dispatch(createTask({ title, description }));
+  }
+
+  onStatusChange = (id, status) => {
+    this.props.dispatch(editTask(id, { status }));
+  };
+}
+
+function mapStateToProps(state) {
+  return {
+    tasks: state.tasks,
+  }
+}
+
+export default connect(mapStateToProps)(App);
